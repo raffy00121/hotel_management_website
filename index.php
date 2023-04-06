3ebef07d7b9b3d8fa52e4c1616cee6f39e663a3c
Raffy Dela Cruz <radelacruz00121@usep.edu.ph>
2023-04-06 22:46:26 +0800

    Completed the header for homepage

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600;700&family=Raleway:wght@300;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
   <style>
     * 
     {
@@ -16,6 +17,16 @@
     .h-font
     {
        font-family: 'Merienda', cursive;
+    }
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    /* Firefox */
+    input[type=number] {
+      -moz-appearance: textfield;
     }
   </style>  
   <title>Hotel Management</title>
@@ -47,11 +58,111 @@
         </li>
       </ul>
       <div class="d-flex">
-        <button class="btn btn-outline-success shadow-none" type="submit">Search</button>
+        <button type="button" class="btn btn-outline-dark shadow-none me-lg-2 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+          Login
+        </button>
+        <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+          Register
+        </button>
       </div>
     </div>
   </div>
 </nav>
+<!-- Login Modal -->
+<div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form>
+        <div class="modal-header">
+          <h5 class="modal-title d-flex align-items-center">
+            <i class="bi bi-person-circle fs-3 me-2"></i>
+            User Login
+          </h5>
+          <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+            <label class="form-label">Email address</label>
+            <input type="email" class="form-control shadow-none">
+          </div>
+          <div class="mb-4">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control shadow-none">
+          </div>
+          <div class="d-flex align-items-center justify-content-between mb-2">
+            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+            <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<!-- Register Modal -->
+<div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <form>
+        <div class="modal-header">
+          <h5 class="modal-title d-flex align-items-center">
+            <i class="bi bi-person-lines-fill fs-3 me-2"></i>
+            User Registration
+          </h5>
+          <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <span class="badge bg-light text-dark mb-3 text-wrap lh-base">
+            Note: Your details must match with your ID (National ID, passport, driving license, etc.)
+            that will be required during check-in 
+          </span>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-6 ps-0 mb-3">
+                <label class="form-label">Name</label>
+                <input type="text" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 p-0" mb-3>
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 ps-0 mb-3">
+                <label class="form-label">Phone Number</label>
+                <input type="number" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 p-0 mb-3">
+                <label class="form-label">Picture</label>
+                <input type="file" class="form-control shadow-none">
+              </div>
+              <div class="col-md-12 p-0 mb-3">
+                <label class="form-label">Address</label>
+                <textarea class="form-control shadow-none" rows="1"></textarea>
+              </div>
+              <div class="col-md-6 ps-0 mb-3">
+                <label class="form-label">Pincode</label>
+                <input type="number" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 p-0 mb-3">
+                <label class="form-label">Date of birth</label>
+                <input type="date" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 ps-0 mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control shadow-none">
+              </div>
+              <div class="col-md-6 p-0 mb-3">
+                <label class="form-label">Confirm Password</label>
+                <input type="date" class="form-control shadow-none">
+              </div>
+            </div>
+          </div>
+          <div class="text-center my-1">
+            <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
   
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
